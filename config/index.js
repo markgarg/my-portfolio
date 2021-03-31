5cf5c928444904e5acc6d4c0a8ee8752fab5d2a8
Rohit Macherla <rohit.macherla2@sky.uk>
2021-03-31 17:32:29 +0100

    fix: updates medium url as medium changed it

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
   // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
   // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
   mediumRssFeed:
-    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40rohit.macherla",
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40rohit-macherla",
   // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
 
   shownArticles: 3,
@@ -59,7 +59,7 @@ module.exports = {
     },
     {
       name: "Medium",
-      url: "https://medium.com/@rohit.macherla",
+      url: "https://rohit-macherla.medium.com/",
     },
     {
       name: "Github",
