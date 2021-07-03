04149118b56ce182c724271f4ab3be3c0b9c37b2
Rohit Macherla <rohit.macherla@gmail.com>
2021-07-03 18:34:45 +0100

    feat: updating sitemap description

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,9 +67,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Rohit Macherla's blog`,
+        short_name: `rohit-macherla-blog`,
+        description: `Rohit Macherla's portfolio and blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
