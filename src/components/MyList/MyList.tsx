8ca9fce2483e60a0190a89c28bb8adecebaa685e
Rohit Macherla <rohit.macherla@gmail.com>
2021-07-10 22:26:21 +0100

    bit of styling

--- src/components/MyList/MyList.tsx
+++ src/components/MyList/MyList.tsx
@@ -31,13 +31,15 @@ const MyList = ({ items }: MyListProps) => {
     return (
         <ul sx={{ pl: 10 }}>
             {items.map((item, i) => (
-                <li key={i} sx={{ listStyle: `none` }}>
-                    <div sx={{ display: `inline`, fontWeight: `bold`, mr: 10 }}>{ getIconName(item.icon) }</div>
-                    <div sx={{ display: `inline`, fontSize: [1, 1, 2]}}>
-                        <strong>{item.heading}</strong>
-                    </div>
-                    <div sx={{ display: getDisplayForBody(item.heading), fontSize: [1, 1, 18], pl: 20 }}>
-                        {item.body}
+                <li key={i} sx={{ listStyle: `none`, pb: 10 }}>
+                    <div sx={{ display: `inline`, fontWeight: `bold` }}>{ getIconName(item.icon) }</div>
+                    <div sx={{ display: `inline`, ml: 10 }}>
+                        <div sx={{ display: `inline`, fontSize: [1, 1, 2]}}>
+                            <strong>{item.heading}</strong>
+                        </div>
+                        <div sx={{ display: getDisplayForBody(item.heading), fontSize: [1, 1, 18] }}>
+                            {item.body}
+                        </div>
                     </div>
                 </li>
             ))}
